9dda82c303c593b9a08812a1216f1d536822cb6c
Aleksey525 <alex_tolchin@mail.ru>
2024-02-01 22:38:20 +0300

    upgrade script

--- script.py
+++ script.py
@@ -8,45 +8,53 @@ from django.core.exceptions import MultipleObjectsReturned
 import random
 
 
-praises = ['Молодец!', 'Отлично!', 'Великолепно!', 'Прекрасно!', 'Талантливо!']
+PRAISES = ['Молодец!', 'Отлично!', 'Великолепно!', 'Прекрасно!', 'Талантливо!']
+
+
+def get_user_data(schoolkid):
+    try:
+        user_data = Schoolkid.objects.get(full_name__contains=schoolkid)
+        return user_data
+    except ObjectDoesNotExist:
+        print('Опечатка. Или ученика с таким именем нет в базе. Программа завершена.')
+    except MultipleObjectsReturned:
+        print('Найдено несколько учеников с таким именем, уточните запрос. Программа завершена.')
 
 
 def fix_marks(schoolkid):
-    child = Schoolkid.objects.get(full_name__contains=schoolkid)
-    marks = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
-    for mark in marks:
-        mark.points = '5'
-        mark.save()
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
 
 
 def remove_chastisements(schoolkid):
-    child = Schoolkid.objects.get(full_name__contains=schoolkid)
-    sements = Chastisement.objects.filter(schoolkid=child)
-    for sement in sements:
-        sement.delete()
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
 def create_commendation(schoolkid, subject):
-    praise = random.choice(praises)
-    child = Schoolkid.objects.get(full_name__contains=schoolkid)
-    less = Lesson.objects.get(year_of_study=child.year_of_study,
-                              group_letter=child.group_letter, subject__title__contains=subject).first()
-    Commendation.objects.create(schoolkid=child, text=praise, created=less.date,
-                                teacher=less.teacher, subject=less.subject)
+    praise = random.choice(PRAISES)
+    try:
+        lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
+                                       group_letter=schoolkid.group_letter, subject__title__contains=subject).first()
+        Commendation.objects.create(schoolkid=schoolkid, text=praise, created=lesson.date,
+                                    teacher=lesson.teacher, subject=lesson.subject)
+    except AttributeError:
+        print('Опечатка. Или предмета с таким названием нет в базе. Программа завершена.')
 
 
 def run(*args):
-    print(args[0])
-    try:
+    if len(args) == 0:
+        print('Вы ввели пустую строку')
+    else:
+        child = get_user_data(args[0])
         if len(args) == 1:
-            fix_marks(args[0])
-            remove_chastisements(args[0])
+            fix_marks(child)
+            remove_chastisements(child)
         else:
-            create_commendation(args[0], args[1])
-    except ObjectDoesNotExist:
-        print('Опечатка. Или ученика с таким именем нет в базе. Программа Завершена.')
-    except MultipleObjectsReturned:
-        print('Найдено несколько учеников с таким именем, уточните запрос. Программа завершена')
+            subject = args[1]
+            create_commendation(child, subject)
+
+
+if __name__ == '__main__':
+    run()
 
 
 
